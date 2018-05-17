e96dba4901f21f818517b290e9a769c701920cfc
Erin Olsen <erin.angelita@gmail.com>
2018-05-17 16:52:05 -0600

    added image file to App.js, added image files for future use, and styled text in app.js

--- App.js
+++ App.js
@@ -1,19 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.titleText}>Cats & Hats</Text>
+        <Image source={require('./images/pizza_cat.jpg')}  />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  titleText: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
